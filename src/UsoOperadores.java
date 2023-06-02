45f5e3e25f18b3f6198cf0bc68625622f0aeeda8
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-06-02 00:19:45 -0500

    uso operadores

---
+++ src/UsoOperadores.java
@@ -0,0 +1,11 @@
+public class UsoOperadores {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 8;
+
+        num2++;
+
+        int resultado = num1 + num2;
+        System.out.println(resultado);
+    }
+}
