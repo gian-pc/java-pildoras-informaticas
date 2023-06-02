ed6051ef94b358984070a745b230cb98d28c3207
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-06-02 00:52:42 -0500

    constantes y operadores II

---
+++ src/Concatenacion.java
@@ -0,0 +1,6 @@
+public class Concatenacion {
+    public static void main(String[] args) {
+        double salario = 1850.55;
+        System.out.println("El salario de Manuel es: " + (salario + 300) + " a día de hoy");
+    }
+}
