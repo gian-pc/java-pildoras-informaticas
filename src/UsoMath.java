c78980d0fecf5acc9add97c8920ed39aa303fc61
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-06-03 11:38:58 -0500

    clase math

--- src/UsoMath.java
+++ src/UsoMath.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class UsoMath {
+public class UsoMath {
+    public static void main(String[] args) {
+
+        System.out.println("Raiz cuadrada: " + Math.sqrt(9));
+        System.out.println("Redondear: " + Math.round(5.4));
+
+    }
 }
