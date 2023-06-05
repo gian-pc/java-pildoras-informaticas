8b848027d34e4c9ea509a3e82f256b46170ec837
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-06-05 14:35:18 -0500

    paquetes

---
+++ src/Paquetes.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Paquetes {
+    public static void main(String[] args) {
+
+        String frase;
+        Button miBoton;
+        Frame miVentana;
+        Color miColor;
+        Scanner miEscaner;
+
+    }
+}
