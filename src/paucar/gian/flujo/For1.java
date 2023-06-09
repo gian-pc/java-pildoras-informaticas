cd4987f9c6e4c6882116d0279c2a9187fad2ed1c
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-06-08 19:37:36 -0500

    for

---
+++ src/paucar/gian/flujo/For1.java
@@ -0,0 +1,14 @@
+package paucar.gian.flujo;
+
+public class For1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <3;i++) {
+            System.out.println(i+" mmm");
+            for (int j = 1; j < 3; j++) {
+                System.out.println(j+" hola");
+            }
+        }
+
+    }
+}
