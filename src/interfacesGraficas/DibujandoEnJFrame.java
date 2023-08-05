1a2284c0262546e7ffe8b07e537a563addf566c4
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-08-05 13:05:33 -0500

    graphics2d

--- src/interfacesGraficas/DibujandoEnJFrame.java
+++ src/interfacesGraficas/DibujandoEnJFrame.java
@@ -2,6 +2,7 @@ package interfacesGraficas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class DibujandoEnJFrame {
     public static void main(String[] args) {
@@ -27,5 +28,14 @@ class LaminaDibujos extends JPanel{
         g.drawRect(20,20,125,75);
         g.drawLine(20,20,145,95);
         g.fillRect(20,200,125,75);
+
+        Graphics2D g2 = (Graphics2D)g;
+        Rectangle2D rectangulo = new Rectangle2D.Double(200,100,200,150);
+
+        float midash[]={10.0f};
+        //BasicStroke milapiz = new BasicStroke(4);
+        BasicStroke milapiz = new BasicStroke(4,BasicStroke.CAP_BUTT,BasicStroke.JOIN_MITER,10,midash,0);
+        g2.setStroke(milapiz);
+        g2.draw(rectangulo);
     }
 }
